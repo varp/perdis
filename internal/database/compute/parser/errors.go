b4a28c2b14753fa9b5b97207be5f6790ed7e076f
Vardan Pogosian <vardan.pogosyan@gmail.com>
2024-07-07 22:12:40 +0300

    feat: parser implemented without tests

---
+++ internal/database/compute/parser/errors.go
@@ -0,0 +1,7 @@
+package parser
+
+import "errors"
+
+var (
+	errParserInvalidCharacter = errors.New("invalid character")
+)
--- internal/database/compute/parser/parser.go
+++ internal/database/compute/parser/parser.go
@@ -1,1 +1,30 @@
 package parser
+
+import "log/slog"
+
+type Parser struct {
+	logger *slog.Logger
+	sm     *stateMachine
+}
+
+func NewParser(logger *slog.Logger) *Parser {
+	return &Parser{
+		logger: logger,
+		sm:     newStateMachine(),
+	}
+}
+
+func (p *Parser) ParseQuery(query string) ([]string, error) {
+	tokens, err := p.sm.parse(query)
+	if err != nil {
+		return nil, err
+	}
+
+	p.logger.Debug(
+		"parsed query",
+		slog.String("query", query),
+		slog.Any("tokens", tokens),
+	)
+
+	return tokens, nil
+}
